cfe53034e0de732fcfda1f5a2cb415f0a6af9e1a
Adeleon5 <capri.gerry@gmailcom>
2020-08-06 13:13:33 -0600

    Se creo una carpeta Chome y el componente Home dentro de ella

--- PlataformaWeb/src/app/app.module.ts
+++ PlataformaWeb/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './Cprincipal/app.component';
 import { LoginComponent } from './CLogin/login/login.component';
+import { HomeComponent } from './Chome/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
