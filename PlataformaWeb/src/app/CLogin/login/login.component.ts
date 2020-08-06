890159009b37a69c2983184a33c349c366c8b3cc
Adeleon5 <capri.gerry@gmailcom>
2020-08-06 17:32:20 -0600

    Se agrega codigo al servicio para consumir API de JAva

--- PlataformaWeb/src/app/CLogin/login/login.component.ts
+++ PlataformaWeb/src/app/CLogin/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiRestSBService } from 'src/app/Sapirest/api-rest-sb.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  password: string;
+  message: any;
+
+  constructor(private service:ApiRestSBService, private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  doLogin(){
+    let resp = this.service.login(this.username, this.password);
+    console.log(resp);
+    if(resp){
+      this.router.navigate(["/home"]);
+    }else{
+      alert("Usuairo o contraseña invalidos, intente de nuevo")
+    }
+    
   }
 
 }
--- PlataformaWeb/src/app/Sapirest/api-rest-sb.service.ts
+++ PlataformaWeb/src/app/Sapirest/api-rest-sb.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiRestSBService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public login(username:string, password:string){
+    const headers= new HttpHeaders({Authorization: 'Basic '+btoa(username+':'+password)})
+    return this.http.get("http://localhost:8080/usuario/login",{headers, responseType:'text' as 'json'})
+  }
 }
