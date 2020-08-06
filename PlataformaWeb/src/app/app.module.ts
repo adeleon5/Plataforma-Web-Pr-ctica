ca6c7009ae61307247d72ddbee48b80ea4b86922
Adeleon5 <capri.gerry@gmailcom>
2020-08-06 13:11:47 -0600

    HTML para Login y CSS

--- PlataformaWeb/src/app/app.module.ts
+++ PlataformaWeb/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './Cprincipal/app.component';
 import { LoginComponent } from './CLogin/login/login.component';
 
 @NgModule({
